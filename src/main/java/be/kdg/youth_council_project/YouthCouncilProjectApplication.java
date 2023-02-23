153c16016108351124722b791930cebcf7fbe3a3
Filip Nowak <filip.nowak@student.kdg.be>
2023-02-23 08:25:31 +0000

    Filip/init

---
+++ src/main/java/be/kdg/youth_council_project/YouthCouncilProjectApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.youth_council_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthCouncilProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouthCouncilProjectApplication.class, args);
+    }
+
+}
