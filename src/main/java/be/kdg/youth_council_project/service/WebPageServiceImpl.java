b9d9ad50e3c54046971b7673acd05be34b049c7a
Filip Nowak <filip.nowak@student.kdg.be>
2023-03-10 11:18:23 +0100

    chore: setting up thymeleaf/fixing some minor bugs/creating a dataseeder TODO: FILL IT UP WITH FAKER DATA

--- src/main/java/be/kdg/youth_council_project/service/WebPageServiceImpl.java
+++ src/main/java/be/kdg/youth_council_project/service/WebPageServiceImpl.java
@@ -3,5 +3,5 @@ package be.kdg.youth_council_project.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class WebPageServiceImpl implements WebPageService
+public class WebPageServiceImpl implements WebPageService{
 }
