8890afa31e054466b9e86b34173fd1932fc68db4
pashioya <paul.ashioya@student.kdg.be>
2023-03-20 16:31:45 +0100

    basic index page and functionality

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,24 @@
+function scrollFunction1() {
+    let e = document.getElementById("about-us");
+    e.scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+        inline: "center",
+    });
+}
+function scrollFunction2() {
+    let e = document.getElementById("news");
+    e.scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+        inline: "center",
+    });
+}
+function scrollFunction3() {
+    let e = document.getElementById("youth-councils");
+    e.scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+        inline: "center",
+    });
+}
