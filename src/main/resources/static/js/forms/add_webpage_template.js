e7eb0be1fab694f8cbbbb5cc0650bac6a4043d0c
Filip Nowak <filip.nowak@student.kdg.be>
2023-04-17 15:47:32 +0200

    remmove unnecessary js

--- src/main/resources/static/js/forms/add_webpage_template.js
+++ src/main/resources/static/js/forms/add_webpage_template.js
@@ -1,42 +1,4 @@
-const textAreas = document.getElementsByTagName("textarea");
-
-for (const textArea of textAreas) {
-    textArea.addEventListener("keyup", (event) => {
-        textAreaAdjust((textArea));
-    })
-}
-
-function textAreaAdjust(element) {
-    element.style.height = "0px";
-    element.style.height = element.scrollHeight + "px";
-}
-
-function setColumnClass() {
-    if (window.innerWidth <= 576) { // check if the viewport width is mobile sized
-        let divs = document.getElementsByClassName("col-6"); // select all div elements with class "col-6"
-        if (divs != null) {
-            for (let div of divs) {
-                div.classList.remove("col-6"); // remove the class "col-6"
-                div.classList.add("col-12"); // add the class "col-12"
-            }
-        }
-    } else { // revert back to "col-6" class for larger viewports
-        let divs = document.getElementsByClassName("col-12"); // select all div elements with class "col-12"
-        if (divs != null) {
-            for (let div of divs) {
-                div.classList.remove("col-12"); // remove the class "col-12"
-                div.classList.add("col-6"); // add the class "col-6"
-            }
-        }
-    }
-    for (const textArea of textAreas) {
-        textAreaAdjust(textArea);
-    }
-}
-
-window.addEventListener("resize", setColumnClass);
-
-const submitButton = document.getElementById("submit-template");
+tButton = document.getElementById("submit-template");
 
 submitButton.addEventListener("click", submitTemplate);
 
