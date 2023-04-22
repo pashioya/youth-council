782ba36225559281b3f43f7f27d9810a5e2d2a41
Filip Nowak <filip.nowak@student.kdg.be>
2023-04-22 11:12:55 +0000

    Filip/redesign yc page

--- src/main/java/be/kdg/youth_council_project/service/WebPageService.java
+++ src/main/java/be/kdg/youth_council_project/service/WebPageService.java
@@ -1,7 +1,6 @@
 package be.kdg.youth_council_project.service;
 
 import be.kdg.youth_council_project.domain.platform.YouthCouncil;
-import be.kdg.youth_council_project.domain.webpage.HomePage;
 import be.kdg.youth_council_project.domain.webpage.WebPage;
 
 public interface WebPageService {
@@ -9,5 +8,5 @@ public interface WebPageService {
 
     WebPage updateWebPage(long youthCouncilId, WebPage map);
 
-    HomePage createHomePageOfYouthCouncil(YouthCouncil youthCouncil);
+    WebPage createHomePageOfYouthCouncil(YouthCouncil youthCouncil);
 }
--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -8,11 +8,17 @@ insert into youth_council (slug, name, municipality_id, logo) values ('brusselsy
 
 
 -- WEBPAGE
-insert into web_page (page_type, title, action_points_enabled,activities_enabled,call_for_ideas_enabled,
-                     call_to_complete_questionnaire_enabled,election_information_enabled,news_items_enabled)
-values ('HOME', 'Antwerp Youth Council Homepage',True,True,True,True,True,True);
+insert into web_page (title, action_points_enabled,activities_enabled,call_for_ideas_enabled,
+                     call_to_complete_questionnaire_enabled,election_information_enabled,news_items_enabled,youth_council_id)
+values ('Antwerp Youth Council Homepage',True,True,True,True,True,True,1);
+INSERT INTO section(body, heading, webpage_id)
+VALUES ('hi', 'Who are we?', 1);
+INSERT INTO section(body, heading, webpage_id)
+VALUES ('I am writing this out to test whether the div wraps nicely and hopefully i wont have to change any of the ' ||
+        'styling because this is pretty long. This is another sentence Im writing and now i think ill just copy a ' ||
+        'word over and over again
+         ', 'What is this?', 1);
 
-insert into home_page (id, youth_council_id) values (1, 1);
 
 
 -- -- QUESTION + QUESTIONNAIRE
@@ -83,15 +89,18 @@ values ('2022-06-05 12:55:44','Create tutoring program for Antwerp middle school
 
 -- -- ACTIVITY
 --
--- insert into activity values ('This is an activity, Lets move!','2023-06-05 12:55:44','Title! Running and moving','2023-06-02 12:55:44',1);
-INSERT INTO ACTIVITY (DESCRIPTION, END_DATE, NAME, START_DATE, YOUTH_COUNCIL_ID) VALUES ('Hiking Trip', '2023-05-01 12:55:44', 'Explorers Club', '2023-04-30 4:00:44', 1);
-INSERT INTO ACTIVITY (DESCRIPTION, END_DATE, NAME, START_DATE, YOUTH_COUNCIL_ID) VALUES ('Hiking Trip', '2023-05-01 12:55:44', 'Explorers Club', '2023-04-30 4:00:44', 2);
+-- insert into activity values (1,'This is an activity, Lets move!','2023-06-05 12:55:44','Title! Running and moving','2023-06-02 12:55:44',1);
 --
 -- NEWS ITEM
 
-INSERT INTO news_item(content, created_date, image, title, author_id, youth_council_id)
-VALUES ('Hello, this is an announcement about this being a news item', '2022-06-05 12:55:44', 'https://hips' ||
-                                                                                              '.hearstapps.com/hmg-prod/images/how-to-keep-ducks-call-ducks-1615457181.jpg?crop=0.669xw,1.00xh;0.166xw,0&resize=1200:*', 'This is an announcement!', 2, 1);
+INSERT INTO news_item(content, created_date, title, author_id, youth_council_id)
+VALUES ('Hello, this is an announcement about this being a news item', '2022-06-05 12:55:44', 'This is an announcement!', 2, 1);
+INSERT INTO news_item(content, created_date, title, author_id, youth_council_id)
+VALUES ('Albert heijn chicken is now 50 cents more expensive', '2022-06-05 12:55:44', 'This is an announcement!', 2, 1);
+INSERT INTO news_item(content, created_date, title, author_id, youth_council_id)
+VALUES ('Albert heijn chicken is now 50 cents more expensive', '2022-06-05 12:55:44', 'This is an announcement!', 2, 1);
+INSERT INTO news_item(content, created_date, title, author_id, youth_council_id)
+VALUES ('Albert heijn chicken is now 50 cents more expensive', '2022-06-05 12:55:44', 'This is an announcement!', 2, 1);
 -- NEWS ITEM LIKE
 insert into news_item_like (liked_date_time, user_id, news_item_id) values ('2023-10-10 12:00:00', 1, 1);
 -- -- IDEA
