5e214f40a7e631947f7ed9719da0f6c20f0d21ff
Paul Ashioya <John.ashioya@gmail.com>
2023-04-24 22:34:42 +0200

    fixing tenant account creation

--- src/main/java/be/kdg/youth_council_project/domain/platform/Membership.java
+++ src/main/java/be/kdg/youth_council_project/domain/platform/Membership.java
@@ -3,7 +3,10 @@ package be.kdg.youth_council_project.domain.platform;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.*;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import java.time.LocalDateTime;
 
 @ToString
@@ -16,12 +19,10 @@ public class Membership {
 
     @EmbeddedId
     private MembershipId membershipId;
-
     @Enumerated(EnumType.STRING)
     private Role role;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDateTime dateJoined;
-
 
 
 }
--- src/main/java/be/kdg/youth_council_project/service/UserServiceImpl.java
+++ src/main/java/be/kdg/youth_council_project/service/UserServiceImpl.java
@@ -1,14 +1,19 @@
 package be.kdg.youth_council_project.service;
 
 import be.kdg.youth_council_project.domain.platform.Membership;
+import be.kdg.youth_council_project.domain.platform.MembershipId;
+import be.kdg.youth_council_project.domain.platform.Role;
 import be.kdg.youth_council_project.domain.platform.User;
 import be.kdg.youth_council_project.repository.MembershipRepository;
 import be.kdg.youth_council_project.repository.UserRepository;
+import be.kdg.youth_council_project.repository.YouthCouncilRepository;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -16,15 +21,20 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-
     private final MembershipRepository membershipRepository;
+    private final YouthCouncilRepository youthCouncilRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @Override
-    public User saveUser(User user) {
+    public User saveUser(User user, long youthCouncilId) {
         LOGGER.info("UserService is running saveUser");
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         User savedUser = userRepository.save(user);
+        MembershipId membershipId = new MembershipId(youthCouncilRepository.getReferenceById(youthCouncilId), savedUser);
+        Membership membership = new Membership(membershipId, Role.USER, LocalDateTime.now());
+        membershipRepository.save(membership);
         LOGGER.debug("User with name {} saved", savedUser.getUsername());
         return savedUser;
     }
@@ -39,6 +49,7 @@ public class UserServiceImpl implements UserService {
         }
         return user;
     }
+
 
     @Override
     public List<User> getAllUsers() {
