a7b237240e88643e0bde6e27fec34a11f43e7abb
Filip Nowak <filip.nowak@student.kdg.be>
2023-04-27 13:45:18 +0200

    edited endpoint

--- src/main/java/be/kdg/youth_council_project/controller/api/GeneralAdminDashboardController.java
+++ src/main/java/be/kdg/youth_council_project/controller/api/GeneralAdminDashboardController.java
@@ -2,9 +2,6 @@ package be.kdg.youth_council_project.controller.api;
 
 import be.kdg.youth_council_project.controller.api.dtos.UserDto;
 import be.kdg.youth_council_project.domain.platform.Membership;
-import be.kdg.youth_council_project.domain.platform.Role;
-import be.kdg.youth_council_project.domain.platform.User;
-import be.kdg.youth_council_project.domain.platform.YouthCouncil;
 import be.kdg.youth_council_project.repository.MembershipRepository;
 import be.kdg.youth_council_project.repository.UserRepository;
 import be.kdg.youth_council_project.repository.YouthCouncilRepository;
@@ -12,15 +9,12 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Member;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
@@ -34,13 +28,13 @@ public class GeneralAdminDashboardController {
 
     private final ModelMapper modelMapper;
     @GetMapping("youth-councils/{id}/admins")
-    public ResponseEntity<List<Membership>> getYouthCouncilAdmins(@PathVariable("id") long youthCouncilId) {
+    public ResponseEntity<List<UserDto>> getYouthCouncilAdmins(
+                                                                  @PathVariable("id") long youthCouncilId) {
         LOGGER.info("GeneralAdminDashboardController is running getYouthCouncilAdmins");
-        LOGGER.info("users of yc : ",
-                membershipRepository.findMembersOfYouthCouncilByYouthCouncilId(youthCouncilId).stream().map(Membership::getRole).collect(Collectors.toList()));
-
-        return ResponseEntity.ok(List.of(new Membership()));
-
+        List<Membership> admins = membershipRepository.findAdminsOfYouthCouncilByYouthCouncilId(youthCouncilId);
+        List<UserDto> adminDtos = admins.stream().map(admin -> modelMapper.map(admin.getMembershipId().getUser(),
+                UserDto.class)).toList();
+        return ResponseEntity.ok(adminDtos);
     }
 
 
--- src/main/java/be/kdg/youth_council_project/controller/api/UsersController.java
+++ src/main/java/be/kdg/youth_council_project/controller/api/UsersController.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/users")
-public class UsersController {
+public class UsersController  {
 
 
     private final IdeaService ideaService;
