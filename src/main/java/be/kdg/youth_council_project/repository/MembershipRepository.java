484048f44aad77db5a0fbd13259110aeabb70ffa
Filip Nowak <filip.nowak@student.kdg.be>
2023-04-27 13:45:18 +0200

    edited endpoint

--- src/main/java/be/kdg/youth_council_project/repository/MembershipRepository.java
+++ src/main/java/be/kdg/youth_council_project/repository/MembershipRepository.java
@@ -1,7 +1,6 @@
 package be.kdg.youth_council_project.repository;
 
 import be.kdg.youth_council_project.domain.platform.Membership;
-import be.kdg.youth_council_project.domain.platform.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -22,4 +21,6 @@ public interface MembershipRepository extends JpaRepository<Membership, Long> {
     @Query(value = "SELECT * FROM membership m WHERE m.youth_council_id =?1", nativeQuery = true)
     List<Membership> findMembersOfYouthCouncilByYouthCouncilId(long youthCouncilId);
 
+    @Query(value = "SELECT * FROM membership m WHERE m.youth_council_id =?1 AND m.role = 'YOUTH_COUNCIL_ADMINISTRATOR'", nativeQuery = true)
+    List<Membership> findAdminsOfYouthCouncilByYouthCouncilId(long youthCouncilId);
 }
