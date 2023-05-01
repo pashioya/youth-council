e33ae04feda87d90ce21dce0ae8ecb3a8e3cbf14
Paul Ashioya <John.ashioya@gmail.com>
2023-05-01 20:21:46 +0200

    added municipality classes and removed unused files

--- src/main/java/be/kdg/youth_council_project/controller/mvc/GeneralAdminDashboardControllerMVC.java
+++ src/main/java/be/kdg/youth_council_project/controller/mvc/GeneralAdminDashboardControllerMVC.java
@@ -2,25 +2,22 @@ package be.kdg.youth_council_project.controller.mvc;
 
 import be.kdg.youth_council_project.controller.mvc.viewmodels.UserViewModel;
 import be.kdg.youth_council_project.controller.mvc.viewmodels.YouthCouncilViewModel;
+import be.kdg.youth_council_project.domain.platform.Municipality;
 import be.kdg.youth_council_project.domain.platform.User;
 import be.kdg.youth_council_project.domain.platform.YouthCouncil;
 import be.kdg.youth_council_project.service.UserService;
 import be.kdg.youth_council_project.service.YouthCouncilService;
+import be.kdg.youth_council_project.service.youth_council_items.MunicipalityService;
 import be.kdg.youth_council_project.tenants.NoTenantController;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.List;
 
 @NoTenantController
@@ -29,6 +26,7 @@ public class GeneralAdminDashboardControllerMVC {
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
     private final YouthCouncilService youthCouncilService;
+    private final MunicipalityService municipalityService;
     private final UserService userService;
     private final ModelMapper modelMapper;
 
@@ -54,17 +52,8 @@ public class GeneralAdminDashboardControllerMVC {
         List<YouthCouncilViewModel> youthCouncilViewModels = youthCouncils.stream().map(
                         youthCouncil -> modelMapper.map(youthCouncil, YouthCouncilViewModel.class))
                 .toList();
-
-        File file = new File("src/main/resources/static/json/Municipalities.json");
-        try {
-            byte[] data = Files.readAllBytes(file.toPath());
-            ByteArrayResource postCodesResource = new ByteArrayResource(data);
-            ObjectMapper mapper = new ObjectMapper();
-            List municipalityList = mapper.readValue(postCodesResource.getInputStream(), List.class);
-            modelAndView.addObject("municipalities", municipalityList);
-        } catch (IOException e) {
-            LOGGER.error("Failed to read file: {}", e.getMessage());
-        }
+        List<Municipality> municipalities = municipalityService.getMunicipalities();
+        modelAndView.addObject("municipalities", municipalities);
         modelAndView.addObject("youthCouncils", youthCouncilViewModels);
         return modelAndView;
     }
