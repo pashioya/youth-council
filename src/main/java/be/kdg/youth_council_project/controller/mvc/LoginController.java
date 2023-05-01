e20065eb8520cdacd377a9c12c1d774ee396b62c
Paul Ashioya <John.ashioya@gmail.com>
2023-05-01 20:53:33 +0200

    fixed signup to use municipalities and postcodes

--- src/main/java/be/kdg/youth_council_project/controller/mvc/LoginController.java
+++ src/main/java/be/kdg/youth_council_project/controller/mvc/LoginController.java
@@ -3,13 +3,13 @@ package be.kdg.youth_council_project.controller.mvc;
 import be.kdg.youth_council_project.controller.api.dtos.NewUserDto;
 import be.kdg.youth_council_project.domain.platform.User;
 import be.kdg.youth_council_project.service.UserService;
+import be.kdg.youth_council_project.service.youth_council_items.MunicipalityService;
 import be.kdg.youth_council_project.tenants.TenantId;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,9 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -28,6 +26,7 @@ public class LoginController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
     private final UserService userService;
+    private final MunicipalityService municipalityService;
 
     @GetMapping("/login")
     public ModelAndView showLogin() {
@@ -67,19 +66,20 @@ public class LoginController {
     }
 
     @GetMapping("/register")
+    @Transactional
     public ModelAndView signUp() {
         LOGGER.info("LoginController is running signUp");
-        File file = new File("src/main/resources/static/json/BelgiumPostCodes.json");
-        try {
-            byte[] data = Files.readAllBytes(file.toPath());
-            ByteArrayResource postCodesResource = new ByteArrayResource(data);
-            ObjectMapper mapper = new ObjectMapper();
-            List postCodesList = mapper.readValue(postCodesResource.getInputStream(), List.class);
-            return new ModelAndView("sign-up", "postCodes", postCodesList);
-        } catch (IOException e) {
-            LOGGER.error("Failed to read file: {}", e.getMessage());
-        }
-        return new ModelAndView("sign-up");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("sign-up");
+
+        List<Integer> postCodes = new ArrayList<>();
+        System.out.println(municipalityService.getMunicipalities().get(0).getPostCodes().get(0).toString());
+        municipalityService.getMunicipalities().forEach(municipality -> {
+            postCodes.addAll(municipality.getPostCodes());
+        });
+        System.out.println(postCodes);
+        modelAndView.addObject("postCodes", postCodes);
+        return modelAndView;
     }
 
 
