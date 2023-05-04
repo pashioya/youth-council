692cf8e08ba120954d0d4ccb743a28789aabf40b
Filip Nowak <filip.nowak@student.kdg.be>
2023-05-04 18:21:43 +0200

    progress

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,15 +6,16 @@
 
 # Variables
 instance_name="youth-council-instance"
+zone="europe-west1-b"
 
 # Remove folder and create it again
-gcloud compute ssh $instance_name --command "rm -rf ~/youth-council && mkdir ~/youth-council"
+gcloud compute ssh --zone=$zone $instance_name --command "rm -rf ~/youth-council && mkdir ~/youth-council"
 
 
 # Copy files to server
-gcloud compute scp --recurse build/libs/*.jar $instance_name:~/youth-council
+gcloud compute scp --zone=$zone --recurse build/libs/fatjar.jar $instance_name:~/youth-council
 
 # Run application
-gcloud compute ssh $instance_name --command "java -jar ~/youth-council/*.jar"
+gcloud compute ssh --zone=$zone $instance_name --command "java -jar ~/youth-council/*.jar"
 
 
