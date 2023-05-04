5882a51fe2a274dce1b243fb3deeb9ac948e66a5
Filip Nowak <filip.nowak@student.kdg.be>
2023-05-04 17:25:24 +0200

    progress

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,16 @@
 
 
 # Variables
-ls build/libs
+instance_name="youth-council-instance"
+
+# Remove folder
+gcloud compute ssh --zone europe-west2-a $instance_name --command "rm -rf ~/youth-council"
+
+
+# Copy files to server
+gcloud compute scp --recurse --zone europe-west2-a build/libs/*.jar $instance_name:~/youth-council
+
+# Run application
+gcloud compute ssh --zone europe-west2-a $instance_name --command "java -jar ~/youth-council/*.jar"
+
+
