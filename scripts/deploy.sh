9ecbcacebbb499e7d8ea0da2545f21486976d26f
Filip Nowak <filip.nowak@student.kdg.be>
2023-05-08 13:10:57 +0200

    working on cicd continues

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,26 +5,51 @@
 
 
 # Variables
-instance_name="youth-council-$(date +%m)"
+instance_name="youth-council-$(date +%s)"
 zone="europe-west1-b"
 project="youth-council-cloud"
 tags="http-server"
+network="default"
 
-# Create firewall rules
+
+# Create firewall rules 8080 http
 gcloud compute firewall-rules list | grep http-server
 if [ $? -eq 1 ]; then
     gcloud compute firewall-rules create http-server --allow tcp:8080
 fi
+
+# Enable access
+gcloud compute firewall-rules create allow-rdp-ingress-from-iap \
+  --direction=INGRESS \
+  --action=allow \
+  --rules=tcp:3389 \
+  --source-ranges=35.235.240.0/20
+
 
 # Create instance
 gcloud compute instances create $instance_name \
 --zone=$zone \
 --project=$project \
 --tags=$tags \
---machine-type=f1-micro
+--machine-type=f1-micro \
+--network=$network \
+--metadata startup-script='
+#!/bin/bash
+apt-get update
+apt-get install -y openjdk17
+gcloud compute scp --zone=$zone --recurse ../build/libs/fatjar.jar ' +"$instance_name"+':~/youth-council/fatjar.jar
+'
+
 # Copy files to server
-gcloud compute scp --zone=$zone --recurse build/libs/fatjar.jar $instance_name:~/youth-council
-gcloud instances add-metadata $instance_name --zone=$zone --metadata startup-script='java -jar ~/youth-council/fatjar.jar &'
+gcloud compute instances add-metadata "$instance_name" --zone=$zone --metadata startup-script='
+#!/bin/bash
+ufw allow 8080/tcp
+java -jar~/youth-council/fatjar.jar &
+'
+
+# Restart instance
+gcloud compute instances reset "$instance_name" --zone=$zone
+
 # Run application
 # END
 
