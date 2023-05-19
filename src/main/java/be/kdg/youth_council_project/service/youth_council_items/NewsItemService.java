9e9d0a04d48d9d5f7c7c420367d02df93d1b1e83
Filip Nowak <filip.nowak@student.kdg.be>
2023-05-19 15:43:26 +0200

    added deletes to all entities

--- src/main/java/be/kdg/youth_council_project/service/youth_council_items/NewsItemService.java
+++ src/main/java/be/kdg/youth_council_project/service/youth_council_items/NewsItemService.java
@@ -1,5 +1,6 @@
 package be.kdg.youth_council_project.service.youth_council_items;
 
+import be.kdg.youth_council_project.controller.api.dtos.youth_council_items.NewsItemDto;
 import be.kdg.youth_council_project.domain.platform.youth_council_items.NewsItem;
 import be.kdg.youth_council_project.domain.platform.youth_council_items.comments.NewsItemComment;
 import be.kdg.youth_council_project.domain.platform.youth_council_items.like.NewsItemLike;
@@ -36,4 +37,6 @@ public interface NewsItemService {
     List<NewsItemComment> getCommentsByUserId(long userId);
 
     List<NewsItemComment> getAllCommentsByYouthCouncilId(long tenantId);
+
+    NewsItemDto mapToDto(NewsItem newsItem);
 }
