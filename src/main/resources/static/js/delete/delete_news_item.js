9977e1078965c760a06e4e496f0eb4bf207df975
Alexia <cismaru.mariaalexia@student.kdg.be>
2023-05-22 04:27:41 +0200

    added js

---
+++ src/main/resources/static/js/delete/delete_news_item.js
@@ -0,0 +1,28 @@
+import {getCsrfInfo} from '../common/utils.js';
+
+export async function deleteNewsItem(id) {
+    return fetch(`/api/news-items/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            ...getCsrfInfo()
+        }
+    });
+}
+
+const deleteButtons = document.querySelectorAll('.delete-news-item');
+deleteButtons.forEach(button => {
+        button.addEventListener('click', async () => {
+            let row = button.parentNode.parentNode;
+            let div = row.parentNode.parentNode;
+            let newsItemDiv = div.parentNode;
+            let id = newsItemDiv.getAttribute('data-news-item-id');
+            let response = await deleteNewsItem(id);
+            if (response.status === 200) {
+                newsItemDiv.remove();
+            } else {
+                alert("Something went wrong");
+            }
+        });
+    }
+);
