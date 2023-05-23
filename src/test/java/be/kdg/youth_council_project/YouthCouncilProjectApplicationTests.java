e458baa94b81b2cda93e4033d63b372032e82621
Filip Nowak <filip.nowak@student.kdg.be>
2023-05-23 20:18:07 +0200

    tests oof

--- src/test/java/be/kdg/youth_council_project/YouthCouncilProjectApplicationTests.java
+++ src/test/java/be/kdg/youth_council_project/YouthCouncilProjectApplicationTests.java
@@ -1,13 +1,9 @@
 package be.kdg.youth_council_project;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class YouthCouncilProjectApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
